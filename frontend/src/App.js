4d8722590aae2cf914884cfe76ffdd45df1b722d
mikiyeme205-creator <mikiyeme205@gmail.com>
2026-01-27 12:48:33 +0300

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import OrderForm from './OrderForm';
+import AdminDashboard from './AdminDashboard';
+
+export default function App() {
+  return (
+    <div>
+      <OrderForm />
+      <hr />
+      <AdminDashboard />
+    </div>
+  );
+}
